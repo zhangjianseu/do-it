a288114c7fb1bbc6695d4a445c0b9972cce6f02a
张舰 <zhangjianzy@qq.com>
2019-04-11 17:22:13 +0800

    [chore] add publish folder

---
+++ docs/precache-manifest.5ef4f34f019baf5940b4285d926e3794.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "08f7be6a3a60ca7df59a",
+    "url": "/static/js/main.afc04343.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "08f7be6a3a60ca7df59a",
+    "url": "/static/css/main.6c4ad568.chunk.css"
+  },
+  {
+    "revision": "95f81f07bd08f3a0e8586b7f8b3dee46",
+    "url": "/index.html"
+  }
+];
